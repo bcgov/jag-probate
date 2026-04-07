a101be3efadd3ed8c84d2ef9e7eff78538da42a5
harmanjit.randhawa <harmanjit.randhawa98@gmail.com>
2026-04-07 11:11:28 -0700

    update

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -105,7 +105,7 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/get-started',
     name: 'NewApplication',
-    component: () => import('../views/Application.vue'),
+    component: () => import('../views/ApplicationForm.vue'),
     meta: { requiresAuth: true },
   },
   {
@@ -117,7 +117,7 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/resume/:submissionId',
     name: 'ResumeApplication',
-    component: () => import('@/views/Application.vue'),
+    component: () => import('@/views/ApplicationForm.vue'),
     meta: { requiresAuth: true },
   },
   {
