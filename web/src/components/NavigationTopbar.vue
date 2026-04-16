f4b3b823820e19303e934d59df1660d207786555
Declan Coughlan <deccough@gmail.com>
2026-04-16 23:35:20 +0000

    Run linting fix

--- web/src/components/NavigationTopbar.vue
+++ web/src/components/NavigationTopbar.vue
@@ -3,17 +3,30 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
       <div class="container-fluid">
         <a class="navbar-brand" href="/">
-          <img class="img-fluid d-none d-lg-block" src="/images/bcid-logo-rev-en.svg" width="177" height="44"
-            alt="B.C. Government Logo" />
-          <img class="img-fluid d-lg-none" src="/images/bcid-symbol-rev.svg" width="63" height="44"
-            alt="B.C. Government Logo" />
+          <img
+            class="img-fluid d-none d-lg-block"
+            src="/images/bcid-logo-rev-en.svg"
+            width="177"
+            height="44"
+            alt="B.C. Government Logo"
+          />
+          <img
+            class="img-fluid d-lg-none"
+            src="/images/bcid-symbol-rev.svg"
+            width="63"
+            height="44"
+            alt="B.C. Government Logo"
+          />
         </a>
 
         <div class="navbar-brand me-auto">
           <h1 class="m-0 text-white d-none d-sm-inline">
             {{ layoutStore.navHeader }}
-            <small class="small d-none d-lg-inline fs-6" v-if="layoutStore.navSubtitle">{{ layoutStore.navSubtitle
-            }}</small>
+            <small
+              class="small d-none d-lg-inline fs-6"
+              v-if="layoutStore.navSubtitle"
+              >{{ layoutStore.navSubtitle }}</small
+            >
           </h1>
           <h1 class="m-0 text-white d-sm-none">Probate</h1>
           <span class="badge ms-2 p-1 ml-2" :class="envBadgeClass">{{
@@ -32,65 +45,69 @@
 </template>
 
 <script setup lang="ts">
-import { useAuthStore, useLayoutStore, useRuntimeConfigStore } from '@/stores';
-import { computed } from 'vue';
+  import {
+    useAuthStore,
+    useLayoutStore,
+    useRuntimeConfigStore,
+  } from '@/stores';
+  import { computed } from 'vue';
 
-const layoutStore = useLayoutStore();
-const authStore = useAuthStore();
-const runtimeConfigStore = useRuntimeConfigStore();
+  const layoutStore = useLayoutStore();
+  const authStore = useAuthStore();
+  const runtimeConfigStore = useRuntimeConfigStore();
 
-/**
- * Logs the user out by clearing the auth store and redirecting
- * to the backend logout endpoint, which signs out of Keycloak.
- */
-const handleLogout = () => {
-  authStore.clearUserInfo();
-  globalThis.location.href = `${import.meta.env.BASE_URL}api/auth/logout`;
-};
+  /**
+   * Logs the user out by clearing the auth store and redirecting
+   * to the backend logout endpoint, which signs out of Keycloak.
+   */
+  const handleLogout = () => {
+    authStore.clearUserInfo();
+    globalThis.location.href = `${import.meta.env.BASE_URL}api/auth/logout`;
+  };
 
-const environment = computed(() => runtimeConfigStore.environmentLabel);
+  const environment = computed(() => runtimeConfigStore.environmentLabel);
 
-// Badge color based on environment
-const envBadgeClass = computed(() => {
-  const env = environment.value;
-  if (env === 'PROD') return 'badge-prod';
-  if (env === 'TEST') return 'badge-test';
-  return 'badge-dev';
-});
+  // Badge color based on environment
+  const envBadgeClass = computed(() => {
+    const env = environment.value;
+    if (env === 'PROD') return 'badge-prod';
+    if (env === 'TEST') return 'badge-test';
+    return 'badge-dev';
+  });
 </script>
 
 <style scoped>
-.navbar-brand h1 {
-  font-size: 1.1rem;
-  font-weight: 600;
-  display: flex;
-  align-items: baseline;
-}
+  .navbar-brand h1 {
+    font-size: 1.1rem;
+    font-weight: 600;
+    display: flex;
+    align-items: baseline;
+  }
 
-.navbar-brand .badge {
-  font-size: 0.6rem;
-  padding: 0.25em 0.5em;
-  font-weight: 700;
-  align-items: center;
-  vertical-align: middle;
-}
+  .navbar-brand .badge {
+    font-size: 0.6rem;
+    padding: 0.25em 0.5em;
+    font-weight: 700;
+    align-items: center;
+    vertical-align: middle;
+  }
 
-.badge-dev {
-  background-color: #fcba19;
-  color: #003366;
-}
+  .badge-dev {
+    background-color: #fcba19;
+    color: #003366;
+  }
 
-.badge-test {
-  background-color: #f9ca54;
-  color: #003366;
-}
+  .badge-test {
+    background-color: #f9ca54;
+    color: #003366;
+  }
 
-.badge-prod {
-  background-color: #dc3545;
-  color: white;
-}
+  .badge-prod {
+    background-color: #dc3545;
+    color: white;
+  }
 
-.navbar .dropdown-menu {
-  width: 250px !important;
-}
+  .navbar .dropdown-menu {
+    width: 250px !important;
+  }
 </style>
